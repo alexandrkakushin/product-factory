77e3708b05fa7440220b28cd8e44abb25be329fe
Alexander Kakushin <alexandrkakushin@yandex.ru>
2018-07-11 04:02:03 +0300

    Добавление кэширования статичных элементов (css, js, fonts)

--- src/main/java/ru/pf/config/MvcConfig.java
+++ src/main/java/ru/pf/config/MvcConfig.java
@@ -1,8 +1,11 @@
 package ru.pf.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 /**
  * @author a.kakushin
@@ -15,5 +18,23 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.
+                addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(3600);
+
+        registry.
+                addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCachePeriod(3600);
+
+        registry.
+                addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/")
+                .setCachePeriod(3600);
     }
 }
