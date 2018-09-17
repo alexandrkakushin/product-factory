59d7bcbfefb2a896b07a565116fc603590e032c2
Alexander Kakushin <alexandrkakushin@yandex.ru>
2018-09-18 01:05:25 +0300

    Добавлена возможность обновления конфигурации (git fetch), ajax

--- src/main/java/ru/pf/controller/api/ApiProjectsController.java
+++ src/main/java/ru/pf/controller/api/ApiProjectsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.pf.controller.development.ConfController;
 import ru.pf.entity.Project;
 import ru.pf.metadata.object.Conf;
 import ru.pf.repository.ProjectsRepository;
@@ -30,24 +31,13 @@ public class ApiProjectsController {
     @Autowired
     ProjectsRepository projectsRepository;
 
+    @Autowired
+    ConfController confController;
+
 
     @GetMapping("/{id}/git/fetch")
-    public ResponseEntity<ResponseGit> gitFetch(@PathVariable(name = "id") Long id) {
-        ResponseGit body = new ResponseGit();
-        body.setSuccess(false);
-
-        projectsRepository.findById(id).ifPresent(
-                (Project project) -> {
-                    try {
-                        projectsService.gitFetch(project);
-                        body.setSuccess(true);
-                    } catch (IOException | GitAPIException ex) {
-                        body.setDescription(ex.getLocalizedMessage());
-                    }
-                }
-        );
-
-        return new ResponseEntity<>(body, HttpStatus.OK);
+    public ResponseEntity<?> gitFetch(@PathVariable(name = "id") Long id) throws IOException {
+        return confController.gitFetch(id);
     }
 
     @GetMapping("/{id}/conf")
@@ -62,19 +52,4 @@ public class ApiProjectsController {
         return new ResponseEntity<>(body, body != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
     }
 
-    @Data
-    static class ResponseGit {
-        private boolean success;
-        private String description;
-
-        public ResponseGit() {}
-
-        public void setSuccess(boolean success) {
-            this.success = success;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-    }
 }
