47e926e4f20f973bf02402bfbe460f971a9b4569
Alexander Kakushin <alexandrkakushin@yandex.ru>
2019-06-13 20:10:12 +0300

    Добавление полей объекта "Группа команд": Отображение, Подсказка, Категория; Реализация представления объекта

--- src/main/java/ru/pf/metadata/object/common/CommandGroup.java
+++ src/main/java/ru/pf/metadata/object/common/CommandGroup.java
@@ -2,6 +2,7 @@ package ru.pf.metadata.object.common;
 
 import lombok.Data;
 import ru.pf.metadata.object.AbstractObject;
+import ru.pf.metadata.object.Catalog;
 import ru.pf.metadata.reader.ObjectReader;
 
 import java.io.IOException;
@@ -14,6 +15,12 @@ import java.nio.file.Path;
 @Data
 public class CommandGroup extends AbstractObject<CommandGroup> {
 
+    private Category category;
+    private Representation representation;
+    private String toolTip;
+
+    // todo: поле Picture сделать отдельным типом данных
+
     public CommandGroup(Path path) {
         super(path);
     }
@@ -24,6 +31,55 @@ public class CommandGroup extends AbstractObject<CommandGroup> {
         if (Files.exists(fileXml)) {
             ObjectReader objReader = new ObjectReader(fileXml);
             objReader.fillCommonField(this);
+
+            String nodeProperties = "/MetaDataObject/" + getMetadataName() + "/Properties/";
+            this.category = Category.valueByName(
+                    objReader.read(nodeProperties + "Category"));
+
+            this.representation = Representation.valueByName(
+                    objReader.read(nodeProperties + "Representation"));
+            this.toolTip = objReader.read(nodeProperties + "ToolTip");
+        }
+    }
+
+    // todo: вынести в отдельный тип данных
+    public enum Representation {
+        PICTURE_AND_TEXT,
+        TEXT,
+        PICTURE,
+        AUTO;
+
+        public static Representation valueByName(String value) {
+            if (value.equalsIgnoreCase("PictureAndText")) {
+                return PICTURE_AND_TEXT;
+            } else if (value.equalsIgnoreCase("Text")) {
+                return TEXT;
+            } else if (value.equalsIgnoreCase("Picture")) {
+                return PICTURE;
+            } else if (value.equalsIgnoreCase("Auto")) {
+                return AUTO;
+            }
+            return null;
+        }
+    }
+
+    public enum Category {
+        NAVIGATION_PANEL,
+        FORM_NAVIGATION_PANEL,
+        ACTIONS_PANEL,
+        FORM_COMMAND_BAR;
+
+        public static Category valueByName(String value) {
+            if (value.equalsIgnoreCase("NavigationPanel")) {
+                return NAVIGATION_PANEL;
+            } else if (value.equalsIgnoreCase("FormNavigationPanel")) {
+                return FORM_NAVIGATION_PANEL;
+            } else if (value.equalsIgnoreCase("ActionsPanel")) {
+                return ACTIONS_PANEL;
+            } else if (value.equalsIgnoreCase("FormCommandBar")) {
+                return FORM_COMMAND_BAR;
+            }
+            return null;
         }
     }
 }
