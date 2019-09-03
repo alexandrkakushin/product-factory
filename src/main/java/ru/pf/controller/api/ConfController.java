45be022465e7ad32bde6b1e913a6f17e6951ce52
Alexander Kakushin <alexandrkakushin@yandex.ru>
2019-09-03 15:42:17 +0300

    Исправление ошибки с получением полей класса ResponseUpdate

--- src/main/java/ru/pf/controller/api/ConfController.java
+++ src/main/java/ru/pf/controller/api/ConfController.java
@@ -1,25 +1,29 @@
 package ru.pf.controller.api;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import ru.pf.entity.Project;
-import ru.pf.metadata.object.Conf;
-import ru.pf.repository.ProjectsRepository;
-import ru.pf.service.ProjectsService;
-import ru.pf.service.ZipService;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.pf.entity.Project;
+import ru.pf.metadata.object.Conf;
+import ru.pf.repository.ProjectsRepository;
+import ru.pf.service.ProjectsService;
+import ru.pf.service.ZipService;
 
 /**
  * @author a.kakushin
@@ -92,7 +96,6 @@ public class ConfController {
         return null;
     }
 
-    @Data
     static class ResponseUpdate{
         private boolean success;
         private String description;
@@ -105,6 +108,14 @@ public class ConfController {
 
         public void setDescription(String description) {
             this.description = description;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public String getDescription() {
+            return description;
         }
     }
 }
