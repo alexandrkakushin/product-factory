39c3a5f0dc77620cbf2c2c27de98c469b0094eeb
Alexander Kakushin <alexandrkakushin@yandex.ru>
2019-10-09 00:19:02 +0300

    Добавление страницы для объекта метаданных Перечисление

--- src/main/java/ru/pf/metadata/object/Enum.java
+++ src/main/java/ru/pf/metadata/object/Enum.java
@@ -1,26 +1,79 @@
 package ru.pf.metadata.object;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.pf.metadata.Module;
+import ru.pf.metadata.reader.ModuleReader;
+import ru.pf.metadata.reader.ObjectReader;
 
 /**
  * @author a.kakushin
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Enum extends AbstractMetadataObject {
 
-    public Enum() {
-    }
+    private boolean useStandardCommands;
+
+    // TODO: чтение основных свойств
+    // <Characteristics/>
+    // <QuickChoice>true</QuickChoice>
+    // <ChoiceMode>BothWays</ChoiceMode>
+    // <DefaultListForm/>
+    // <DefaultChoiceForm/>
+    // <AuxiliaryListForm/>
+    // <AuxiliaryChoiceForm/>
+    // <ListPresentation/>
+    // <ExtendedListPresentation/>
+    // <Explanation/>
+    // <ChoiceHistoryOnInput>Auto</ChoiceHistoryOnInput>
+
+    private Set<EnumValue> values;
+
+    private Module managerModule;
 
     public Enum(Path path) {
         super(path);
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public ObjectReader parse() throws IOException {
+        ObjectReader objReader = super.parse();
+
+        String nodeProperties = "/MetaDataObject/" + getXmlName() + "/Properties/";
+        this.useStandardCommands  = objReader.readBool(nodeProperties + "UseStandardCommands");
+        
+        Path pathExt = super.getFile()
+                .getParent()
+                .resolve(this.getShortName(super.getFile()))
+                .resolve("Ext");
+
+        if (Files.exists(pathExt)) {
+            Path fileManagerModule = pathExt.resolve("ManagerModule.bsl");
+            if (Files.exists(fileManagerModule)) {
+                this.managerModule = ModuleReader.read(fileManagerModule, Module.Type.MANAGER_MODULE);
+            }
+        }
+
+        return objReader;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public String getListPresentation() {
+        return "Перечисления";
+    }    
+
+    @Data
+    public class EnumValue {
+
+        private String name;
+        private String synonym;
+        private String comment;
+
     }
 }
