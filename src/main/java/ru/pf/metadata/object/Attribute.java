46d746f2758b0b61e1d86928b12bee4e034007e9
Alexander Kakushin <alexandrkakushin@yandex.ru>
2020-10-25 20:50:54 +0300

    fix #40

--- src/main/java/ru/pf/metadata/object/Attribute.java
+++ src/main/java/ru/pf/metadata/object/Attribute.java
@@ -1,8 +1,7 @@
 package ru.pf.metadata.object;
 
 import lombok.Data;
-import ru.pf.metadata.reader.ObjectReader;
-import ru.pf.metadata.reader.ReaderException;
+import lombok.EqualsAndHashCode;
 import ru.pf.metadata.type.Type;
 
 import java.util.UUID;
@@ -12,34 +11,12 @@ import java.util.UUID;
  * @author a.kakushin
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class Attribute extends MetadataObject {
 
     private Type type;
 
     public Attribute(Conf conf, UUID uuid, String name, String synonym) {
         super(conf, uuid, name, synonym);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        Attribute attribute = (Attribute) o;
-
-        return type != null ? type.equals(attribute.type) : attribute.type == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (type != null ? type.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public ObjectReader parse() throws ReaderException {
-        return super.parse();
     }
 }
