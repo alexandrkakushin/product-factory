931333af7cda6ede613226702b82babaed79bba7
Alexander Kakushin <alexandrkakushin@yandex.ru>
2020-11-15 13:23:56 +0300

    Добавление страниц для типов метаданных

--- src/main/java/ru/pf/metadata/object/common/CommonTemplate.java
+++ src/main/java/ru/pf/metadata/object/common/CommonTemplate.java
@@ -1,10 +1,10 @@
 package ru.pf.metadata.object.common;
 
-import java.nio.file.Path;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import ru.pf.metadata.object.MetadataObject;
+
+import java.nio.file.Path;
 
 /**
  * @author a.kakushin
@@ -19,6 +19,6 @@ public class CommonTemplate extends MetadataObject {
 
     @Override
     public String getListPresentation() {        
-        return "Общие картинки";
+        return "Общие макеты";
     }        
 }
