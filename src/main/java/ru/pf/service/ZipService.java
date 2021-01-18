f9376cefa5753a1c88d50d557b63052c04f4d2a4
Alexander Kakushin <alexandrkakushin@yandex.ru>
2021-01-19 00:12:38 +0300

    Исправлены ошибки с закрытием потоков

--- src/main/java/ru/pf/service/ZipService.java
+++ src/main/java/ru/pf/service/ZipService.java
@@ -8,20 +8,19 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
+ * Сервис для работы с ZIP-архивами
  * @author a.kakushin
  */
 @Service
 public class ZipService {
 
-    public ByteArrayOutputStream createWithSubdir(Path dir) throws IOException {
+    public ByteArrayOutputStream createWithSubDir(Path dir) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        ZipOutputStream zipOut = new ZipOutputStream(baos);
-        File fileToZip = dir.toFile();
-
-        addDir(fileToZip, fileToZip.getName(), zipOut);
-
-        zipOut.close();
+        try (ZipOutputStream zipOut = new ZipOutputStream(baos)) {
+            File fileToZip = dir.toFile();
+            addDir(fileToZip, fileToZip.getName(), zipOut);
+        }
 
         return baos;
     }
@@ -33,26 +32,29 @@ public class ZipService {
         if (fileToZip.isDirectory()) {
             if (fileName.endsWith("/")) {
                 zipOut.putNextEntry(new ZipEntry(fileName));
-                zipOut.closeEntry();
             } else {
                 zipOut.putNextEntry(new ZipEntry(fileName + "/"));
-                zipOut.closeEntry();
             }
+            zipOut.closeEntry();
+
             File[] children = fileToZip.listFiles();
-            for (File childFile : children) {
-                addDir(childFile, fileName + "/" + childFile.getName(), zipOut);
+            if (children != null) {
+                for (File childFile : children) {
+                    addDir(childFile, fileName + "/" + childFile.getName(), zipOut);
+                }
             }
             return;
         }
-        FileInputStream fis = new FileInputStream(fileToZip);
-        ZipEntry zipEntry = new ZipEntry(fileName);
-        zipOut.putNextEntry(zipEntry);
-        byte[] bytes = new byte[1024];
-        int length;
-        while ((length = fis.read(bytes)) >= 0) {
-            zipOut.write(bytes, 0, length);
-        }
-        fis.close();
-    }
 
+        try (FileInputStream fis = new FileInputStream(fileToZip)) {
+            ZipEntry zipEntry = new ZipEntry(fileName);
+            zipOut.putNextEntry(zipEntry);
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+
+        }
+    }
 }
