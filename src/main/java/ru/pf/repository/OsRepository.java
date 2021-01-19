69a98fec7ca8487139fe1d51f8f8546272227a9e
Alexander Kakushin <alexandrkakushin@yandex.ru>
2021-01-19 12:14:45 +0300

    Добавлен поиск операционной системы по наименованию

--- src/main/java/ru/pf/repository/OsRepository.java
+++ src/main/java/ru/pf/repository/OsRepository.java
@@ -2,11 +2,20 @@ package ru.pf.repository;
 
 import ru.pf.entity.Os;
 
+import java.util.Optional;
+
 /**
  * Репозиторий "Операционные системы"
  * @author a.kakushin
  */
 public interface OsRepository extends PfRepository<Os> {
+
+    /**
+     * Поиск по наименованию
+     * @param name Наименование операционной системы
+     * @return Optional(OS)
+     */
+    Optional<Os> findByName(String name);
 
     @Override
     default Os newInstance() {
